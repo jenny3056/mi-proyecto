f20f121aa4cad22744e14998581ffab0f7aafb36
Jenny TG <jennytamayo991@gmail.com>
2024-05-05 22:24:14 -0500

    Nuevos cambios realizados al proyecto clase 7

---
+++ main/main.js
@@ -0,0 +1,5 @@
+console.log("Hola mundo desde main.js")
+console.log("Este es mi segundo commit");
+console.log("cambio marge")
+console.log("git status")
+console.log("los docentes hacen mucho ruido cuando doy clases")
