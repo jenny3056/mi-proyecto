4873729c93dd6766c52ea744f61de512beef1639
Jenny TG <jennytamayo991@gmail.com>
2024-05-13 21:05:37 -0500

    mi segundo commit

--- main/main.js
+++ main/main.js
@@ -3,3 +3,11 @@ console.log("Este es mi segundo commit");
 console.log("cambio marge")
 console.log("git status")
 console.log("los docentes hacen mucho ruido cuando doy clases")
+console.log("checaout")
+console.log("los docentes hacen mucho ruido cuando doy clases")
+console.log("prueba de checkout")
+console.log("prueba de checkout")
+console.log("prueba de checkout")
+
+
+
