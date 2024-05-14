b31635d87ded3953c2f7d3c2f44556ffa5ad8bc7
Jenny TG <jennytamayo991@gmail.com>
2024-05-14 18:39:12 -0500

    This reverts commit 4873729c93dd6766c52ea744f61de512beef1639.

--- main/main.js
+++ main/main.js
@@ -3,12 +3,3 @@ console.log("Este es mi segundo commit");
 console.log("cambio marge")
 console.log("git status")
 console.log("los docentes hacen mucho ruido cuando doy clases")
-console.log("checaout")
-console.log("los docentes hacen mucho ruido cuando doy clases")
-console.log("prueba de checkout")
-console.log("prueba de checkout")
-console.log("prueba de checkout")
-console.log("prueba de checkout")
-
-
-
